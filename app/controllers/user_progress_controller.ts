99e0e7076f9b767aa7fc70bfc3b5798d38dc137d
Samuel <samuel.varoli@etec.sp.gov.br>
2025-11-30 11:38:49 -0300

    fix: progress conclusion time

--- app/controllers/user_progress_controller.ts
+++ app/controllers/user_progress_controller.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import db from '@adonisjs/lucid/services/db'
 import UserProgress from '#models/user_progress'
+import { DateTime } from 'luxon';
 
 export default class UserProgressController {
     public async cadastrarProgresso({request, response}: HttpContext) {
@@ -28,8 +29,17 @@ export default class UserProgressController {
         if(progressoExistente){
             return response.status(409).send({ message: 'Progresso já cadastrado para este usuário e tópico' });
         }
+
+        const completion_date = DateTime.now();
         
-        await UserProgress.create(data)
+        await UserProgress.create({
+            user_id: data.user_id,
+            level_id: data.level_id,
+            subject_id: data.subject_id,
+            topic_id: data.topic_id,
+            is_completed: data.is_completed,
+            completion_date: completion_date
+        });
         return response.status(201).send({ message: 'Progresso cadastrado com sucesso' });
     }
 
